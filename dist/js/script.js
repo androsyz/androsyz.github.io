e838ee3d1cdeb784205b8c5a46b93749e5439060
Andro Syahreza <androsyahreza3@gmail.com>
2023-03-26 15:04:44 +0700

    add project and view more button

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,11 +4,22 @@ const html = document.querySelector("html");
 const menuButton = document.querySelector("#menu-button");
 const navMenu = document.querySelector("#nav-menu");
 const arrowOnMenu = document.querySelector("#arrow");
+const viewMoreButton = document.querySelector("#view-button");
+const arrowOnViewMore = document.querySelector("#arrow-view-more");
+const additionProject = document.querySelector("#addition-project");
+const textMore = document.querySelector("#text-view-button");
 
 // Click on menu
 menuButton.addEventListener("click", () => {
   arrowOnMenu.classList.toggle("menu-active");
   navMenu.classList.toggle("hidden");
+});
+
+// Click on view more
+viewMoreButton.addEventListener("click", () => {
+  arrowOnViewMore.classList.toggle("more-menu-active");
+  additionProject.classList.toggle("hidden");
+  textMore.innerHTML === "more" ? textMore.innerHTML = "less" : textMore.innerHTML = "more";
 });
 
 // Toggle Button
